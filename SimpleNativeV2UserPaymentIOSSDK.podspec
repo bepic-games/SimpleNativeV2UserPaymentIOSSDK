69fc0f882499785ae11984e59061c4a36539cb20
xxx <xxx@x.com>
2021-10-14 16:04:57 +0800

    read to push

---
+++ SimpleNativeV2UserPaymentIOSSDK.podspec
@@ -0,0 +1,19 @@
+
+Pod::Spec.new do |spec|
+  spec.name = "SimpleNativeV2UserPaymentIOSSDK"
+  spec.version="0.1.0"
+  spec.summary= "Simple SDK for include UserPayment sdk"
+  spec.description= <<-DESC
+  Simple SDK for UserPayment sdk
+  DESC
+  spec.homepage= 'https://github.com/bepic-games/SimpleNativeV2UserPaymentIOSSDK'
+  spec.license= { :type => 'MIT', :file => 'LICENSE' }
+  spec.author = { "yilang" => "yilang@bepic.cc" }
+  spec.ios.deployment_target = "10.0"
+  spec.source = { :git => "https://github.com/bepic-games/SimpleNativeV2UserPaymentIOSSDK.git", :tag => "#{spec.version="0.1.0"
+  spec.source_files= 'SimpleNativeV2UserPaymentIOSSDK/*.{framework}/Headers/*.h'
+  spec.vendored_frameworks = 'SimpleNativeV2UserPaymentIOSSDK/*.{framework}'
+  spec.public_header_files = 'SimpleNativeV2UserPaymentIOSSDK/*.{framework}/Headers/*.h'
+  spec.pod_target_xcconfig = { 'VALID_ARCHS' => 'arm64 armv7 armv7s ' }
+  spec.dependency 'FBSDKLoginKit', '11.2.1'
+end
---
+++ SimpleNativeV2UserPaymentIOSSDK/SimpleNativeUserPayment.framework/Headers/SimpleNativeUserPayment.h
@@ -0,0 +1,70 @@
+//
+//  SimpleNativeUserPayment.h
+//  SimpleNativeUserPayment
+//
+//  Created by qus on 2021/10/8.
+//
+
+#ifndef SimpleNativeUserPayment_h
+#define SimpleNativeUserPayment_h
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <SimpleNativeBase/SimpleNativeBase.h>
+
+//inner model
+
+@interface PaymentParamsResult : NSObject
+@property (nonatomic) long long gameOrderId;
+@property (nonatomic,strong) NSString * paymentMethod;
+@property (nonatomic,strong) NSDictionary * actionInfo;
+@end
+
+@interface ERROR_CODE:NSObject
+
+//连接服务器失败
++(State *)  CLIENT_CONNECTION_ERROR ;
+//请求结果不成功
++(State *) CLIENT_CODE_NOT_SUCCESS ;
+//未登陆
++(State *) CLIENT_HAS_NOT_LOGIN ;
+//登陆方式不支持
++(State *) CLIENT_NOT_SUPPORT_THIS_LOGINTYPE ;
+//登陆参数获取失败
++(State *) CLIENT_GET_LOGIN_PARAMS_FAIL ;
+//找不到品相
++(State *) CLIENT_NOT_FIND_ITEM ;
++(State *) CLIENT_ITEMS_IS_NULL ;
++(State *) CLIENT_SDK_NOT_INIT ;
+//自动登录失败
++(State *) CLIENT_AUTO_LOGIN_FAIL ;
+
+//未知错误
++(State *) CLIENT_UNKNOW_EXCEPTION ;
+
+@end
+
+
+@interface SimpleNativeUserPayment<SimpleSDKUPProtocol>: NSObject
+
+@end
+
+
+@interface SimpleSDKApplication : NSObject
+
++ (instancetype)sharedInstance;
+
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey, id> *)options;
+
+- (void)scene:(UIScene *)scene openURLContexts:(NSSet<UIOpenURLContext *> *)URLContexts;
+@end
+
+//! Project version number for SimpleNativeUserPayment.
+FOUNDATION_EXPORT double SimpleNativeUserPaymentVersionNumber;
+
+//! Project version string for SimpleNativeUserPayment.
+FOUNDATION_EXPORT const unsigned char SimpleNativeUserPaymentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleNativeUserPayment/PublicHeader.h>
+
+
+#endif
